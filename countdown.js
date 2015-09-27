47069b4fbf496fd765a1217056c610f6849140ad
Noam Samuel <noamsml@Noams-MacBook-Air.local>
2015-09-27 09:25:57 -0700

    Add end condition, fix timezone issue, and fix spacing cases

--- countdown.js
+++ countdown.js
@@ -36,11 +36,19 @@
       $('minutes-spacer').style.display = 'none';
     }
 
+    if (!show['seconds'] && !show['minutes'] && !show['hours']) {
+      $('notime-flex').style.display = 'block';
+    }
+
+    if (!show['weeks'] && !show['days']) {
+      $('nodate-flex').style.display = 'block';
+    }
+
     $('prompt').style.display = 'none';
     $('countdown').style.display = 'block';
     $('title').innerText = $('name').value;
 
-    countdownTo = $('date').valueAsNumber;
+    countdownTo = getCountdownToNumber();
 
     setInterval(everySecond, 1000);
     everySecond();
@@ -48,15 +56,34 @@
     return true;
   }
 
+  // I swear this logic gets fucking ridiculous near the edge cases
+  // related to DST. For this purpose, I assume you're not trying to count
+  // to a time near a DST boundary. If you are, this will be off by an hour.
+
+  // And yes, this is a giant ugly honking hack.
+  function getCountdownToNumber() {
+    var dateForOffset = new Date($('date').value);
+    var offset = (dateForOffset.getTimezoneOffset() / 60) * 100;
+
+    var realDate = new Date($('date').value+"-"+zeroPaddedInt(offset, 4))
+    return realDate.getTime()
+  }
+
   function everySecond() {
     var seconds = countdownTo - Date.now();
-    Object.keys(denoms).forEach(function (denom) {
-      if (show[denom]) {
-        $(denom + '-display').innerText = "" +
-          zeroPaddedInt(seconds / denoms[denom], 2);
-        seconds = seconds % denoms[denom];
-      }
-    })
+
+    if (seconds <= 0) {
+      $('actual-countdown').style.display = 'none';
+      $('done').style.display = 'block';
+    } else {
+      Object.keys(denoms).forEach(function (denom) {
+        if (show[denom]) {
+          $(denom + '-display').innerText = "" +
+            zeroPaddedInt(seconds / denoms[denom], 2);
+          seconds = seconds % denoms[denom];
+        }
+      })
+    }
   }
 
   function onLoad() {
